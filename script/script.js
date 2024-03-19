53e984e8b1a806731c10e6dbd294c33befd5af73
Kakonoomoide <rizkiramada128@gmail.com>
2024-03-19 21:40:33 +0700

    featur - change audio to onload

--- script/script.js
+++ script/script.js
@@ -50,17 +50,29 @@ function showNotification() {
   alertElement.style.zIndex = "2";
 }
 
-var audio = document.getElementById("myAudio");
+window.onload = function () {
+  var audio = document.getElementById("myAudio");
 
-function handleVisibilityChange() {
-  if (document.hidden) {
-    // Jika tab web tidak terlihat, jeda pemutaran audio
-    audio.pause();
-  } else {
-    // Jika tab web kembali terlihat, lanjutkan pemutaran audio
-    audio.play();
+  function handleVisibilityChange() {
+    if (document.hidden) {
+      // Jika tab web tidak terlihat, jeda pemutaran audio
+      audio.pause();
+    } else {
+      // Jika tab web kembali terlihat, lanjutkan pemutaran audio
+      audio.play();
+    }
   }
-}
+
+  // Panggil fungsi handleVisibilityChange saat halaman dimuat
+  handleVisibilityChange();
+
+  // Tambahkan pendengar acara visibilitychange ke objek dokumen
+  document.addEventListener("visibilitychange", handleVisibilityChange);
+
+  // Code untuk memainkan audio
+  audio.autoplay = true;
+  audio.loop = true;
+};
 
 // Panggil fungsi handleVisibilityChange saat halaman dimuat
 document.addEventListener("DOMContentLoaded", handleVisibilityChange);
